3cbcf9a8ead4f87f226af768e9aab5e91bd8abbe
Shaez <45568186+sabrinapct@users.noreply.github.com>
2019-04-22 10:09:26 +0200

    Add files via upload

---
+++ pandaExo/script.js
@@ -0,0 +1,6 @@
+
+$('button').on('click', function () {
+    let type = $(this).attr('data-type')
+    let tendance = $(this).attr('data-tendance')
+    alert("son type est "+type+" et sa tendance est"+ tendance);
+})
