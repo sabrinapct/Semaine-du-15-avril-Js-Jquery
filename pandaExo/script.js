c51a07b7376e1be5f06be8ca640bdc025928b087
Shaez <45568186+sabrinapct@users.noreply.github.com>
2019-04-29 10:20:43 +0200

    Update script.js

--- pandaExo/script.js
+++ pandaExo/script.js
@@ -1,5 +1,5 @@
 
-$('button').on('click', function () {
+$('button').on("click", function () {
     let type = $(this).attr("data-type")
     let tendance = $(this).attr("data-tendance")
     alert("son type est "+type+" et sa tendance est"+ tendance);
